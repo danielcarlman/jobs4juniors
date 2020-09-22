8992d4693c48f8fa20745f88f6203106fb0609bd
danielcarlman <daniel.v.carlman@gmail.com>
2020-09-22 15:49:15 -0600

    Added more styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import {
+  ColorModeProvider,
+  CSSReset,
+  Flex,
+  ThemeProvider,
+} from '@chakra-ui/core';
 
 function App() {
   return (
     <ThemeProvider>
       <CSSReset />
-      <h1>React</h1>
+      <ColorModeProvider>
+        <Flex
+          h='100vh'
+          bg='gray.200'
+          direction='column'
+          align='center'
+          justify='center'
+        >
+          <h1>Logo</h1>
+          <h1>Search Bar</h1>
+          <h1>Job Listing</h1>
+        </Flex>
+      </ColorModeProvider>
     </ThemeProvider>
   );
 }
