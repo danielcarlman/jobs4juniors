b52592eb121a7c36dd97a8839358da6843a5b625
danielcarlman <daniel.v.carlman@gmail.com>
2020-09-23 13:14:41 -0600

    Chakra UI setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import {
+  Box,
+  ColorModeProvider,
+  CSSReset,
+  Flex,
+  Stack,
+  ThemeProvider,
+} from '@chakra-ui/core';
 
 function App() {
   return (
-    <div>
-      <h1>React</h1>
-    </div>
+    <ThemeProvider>
+      <CSSReset />
+      <ColorModeProvider>
+        <Flex
+          h='100vh'
+          bg='gray.300'
+          direction='column'
+          align='center'
+          justify='center'
+          p={[2, 4, 6, 8]}
+          textAlign='center'
+        >
+          <Stack spacing={4}>
+            <Box>Logo</Box>
+            <Box>Search Bar</Box>
+            <Box>Job Listing</Box>
+          </Stack>
+        </Flex>
+      </ColorModeProvider>
+    </ThemeProvider>
   );
 }
 
